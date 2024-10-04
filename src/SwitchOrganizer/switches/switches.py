b8280ac8c61d84678d3e0044fd74023c157be504
Стяпин Илья Александрович <sial@svg.ru>
2024-10-04 11:45:15 +0500

    Add exceptions in switches.py

--- src/SwitchOrganizer/switches/switches.py
+++ src/SwitchOrganizer/switches/switches.py
@@ -1,7 +1,7 @@
-from netmiko import ConnectHandler
+from netmiko import ConnectHandler, exceptions
 
-
-def connection_and_upload_conf_to_switch(ip_address_switch: str, username: str, password: str, commands_to_switch: str) -> None:
+# Connecting and sending commands to the switch
+def connection_and_upload_conf_to_switch(ip_address_switch: str, username: str, password: str, commands_to_switch: str) -> str:
 
     for ip_addres_switch in filter(None, ip_address_switch.split('\n')):
         snr_switch_param = {
@@ -11,12 +11,19 @@ def connection_and_upload_conf_to_switch(ip_address_switch: str, username: str, 
             'password': password,
             'port': 22,
         }
-        
-        net_connect = ConnectHandler(**snr_switch_param)
+        try:
+            net_connect = ConnectHandler(**snr_switch_param)
+        except exceptions.NetmikoTimeoutException:
+            print(f"{ip_addres_switch} Введен не корректный IP адрес устройства или устройство не доступно")
+            continue
+        except exceptions.NetmikoAuthenticationException:
+            print(f"{ip_addres_switch} Введены не корретные логин и пароль")
+            break
         net_connect.enable()
         
         for command in commands_to_switch.split('\n'):
-            print(net_connect.send_command_timing(command))
+            net_connect.send_command_timing(command)
     
         net_connect.disconnect()
-    
+
+        print(f"{ip_addres_switch} ======= ОК")
