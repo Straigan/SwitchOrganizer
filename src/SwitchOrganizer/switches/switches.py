2549b6bbcc8aad4a51bc16bb0b1ea01932aaf5f3
Styapin Ilya <istyapin@gmail.com>
2024-10-08 16:53:46 +0500

    Add log in field result_ent

--- src/SwitchOrganizer/switches/switches.py
+++ src/SwitchOrganizer/switches/switches.py
@@ -1,8 +1,11 @@
 from netmiko import ConnectHandler, exceptions
+from tkinter import INSERT
+
 
 # Connecting and sending commands to the switch
-def connection_and_upload_conf_to_switch(ip_address_switch: str, username: str, password: str, commands_to_switch: str) -> str:
-
+def connection_and_upload_conf_to_switch(result_ent, ip_address_switch: str, username: str, password: str, commands_to_switch: str) -> str:
+    
+    result_ent.configure(state='normal')
     for ip_addres_switch in filter(None, ip_address_switch.split('\n')):
         snr_switch_param = {
             'device_type': 'cisco_ios',
@@ -14,16 +17,21 @@ def connection_and_upload_conf_to_switch(ip_address_switch: str, username: str, 
         try:
             net_connect = ConnectHandler(**snr_switch_param)
         except exceptions.NetmikoTimeoutException:
-            print(f"{ip_addres_switch} Введен не корректный IP адрес устройства или устройство не доступно")
+            message = f"{ip_addres_switch} ===== веден не корректный IP адрес устройства или устройство не доступно\n"
+            result_ent.insert(INSERT, message)
             continue
         except exceptions.NetmikoAuthenticationException:
-            print(f"{ip_addres_switch} Введены не корретные логин и пароль")
-            break
+            message = f"{ip_addres_switch} ===== введены не корретные логин и пароль\n"
+            result_ent.insert(INSERT, message)
+            continue
         net_connect.enable()
         
         for command in commands_to_switch.split('\n'):
             net_connect.send_command_timing(command)
-    
+        
         net_connect.disconnect()
 
-        print(f"{ip_addres_switch} ======= ОК")
+        message = f"{ip_addres_switch} ======= ОК\n"
+        result_ent.insert(INSERT, message)
+
+    result_ent.configure(state='disabled')
